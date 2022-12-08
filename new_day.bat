a76d4c518186cc1562e1701fc05aeafc1ba82828
Geilson Fonte <geilsonfonte@gmail.com>
2022-12-07 21:50:49 -0300

    Batch script that creates the files for a new day.

--- new_day.bat
+++ new_day.bat
@@ -3,5 +3,5 @@ copy src\test\kotlin\aoc\year2022\TestDay00.kt src\test\kotlin\aoc\year2022\Test
 copy src\test\resources\year2022\Day00.txt src\test\resources\year2022\Day%1.txt
 copy src\test\resources\year2022\Day00_test.txt src\test\resources\year2022\Day%1_test.txt
 
-powershell -Command "(Get-Content src\main\kotlin\aoc\year2022\Day06.kt).replace('00', '06') | Out-File -encoding utf8 src\main\kotlin\aoc\year2022\Day06.kt"
+powershell -Command "(Get-Content src\main\kotlin\aoc\year2022\Day%1.kt).replace('00', '%1') | Out-File -encoding utf8 src\main\kotlin\aoc\year2022\Day%1.kt"
 powershell -Command "(Get-Content src\test\kotlin\aoc\year2022\TestDay%1.kt).replace('00', '%1') | Out-File -encoding utf8 src\test\kotlin\aoc\year2022\TestDay%1.kt"
