f23422d35b22994a86927a0f6f3fa2ad804f119d
Geilson Fonte <geilsonfonte@gmail.com>
2022-12-07 21:49:05 -0300

    Day 6.

---
+++ src/main/kotlin/aoc/year2022/Day06.kt
@@ -0,0 +1,22 @@
+﻿package aoc.year2022
+
+class Day06 {
+
+    fun part1(input: List<String>): Int {
+        val markerSize = 4
+        return indexOfMarker(input[0], markerSize) + markerSize
+    }
+
+    private fun indexOfMarker(input: String, markerSize: Int): Int {
+        val marker = input.windowed(markerSize)
+            .first { it.toSet().size == markerSize }
+        return input.indexOf(marker)
+    }
+
+    fun part2(input: List<String>): Int {
+        val markerSize = 14
+        return indexOfMarker(input[0], markerSize) + markerSize
+    }
+}
+
+
---
+++ src/test/kotlin/aoc/year2022/TestDay06.kt
@@ -0,0 +1,24 @@
+﻿package aoc.year2022
+
+import readInput
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestDay06 {
+
+    private val puzzle = Day06()
+    private val puzzleName = puzzle.javaClass.simpleName
+
+    @Test fun testInput() {
+        val testInput = readInput(2022, puzzleName + "_test")
+        assertEquals(7, puzzle.part1(testInput))
+        assertEquals(19, puzzle.part2(testInput))
+    }
+
+    @Test fun realInput() {
+        val input = readInput(2022, puzzleName)
+        println(puzzle.part1(input))
+        println(puzzle.part2(input))
+    }
+
+}
